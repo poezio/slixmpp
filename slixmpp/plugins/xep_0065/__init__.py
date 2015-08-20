9c5dd024b190e69583d06fae07db50896a41b454
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2015-08-20 20:02:14 +0100

    Fix the xep_0065 plugin, by rewriting its socks5 implementation.

--- slixmpp/plugins/xep_0065/__init__.py
+++ slixmpp/plugins/xep_0065/__init__.py
@@ -1,5 +1,6 @@
 from slixmpp.plugins.base import register_plugin
 
+from slixmpp.plugins.xep_0065.socks5 import Socks5Protocol
 from slixmpp.plugins.xep_0065.stanza import Socks5
 from slixmpp.plugins.xep_0065.proxy import XEP_0065
 
