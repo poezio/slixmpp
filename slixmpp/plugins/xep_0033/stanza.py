poezio/slixmpp

commit 7cd1cf32ae698965ed488ade17f63afd7d5afedf

Various XEPs: Remove deprecated aliases.

slixmpp/plugins/xep_0033/stanza.py
@@ -118,14 +118,8 @@ for atype in ('all', 'bcc', 'cc', 'noreply', 'replyroom', 'replyto', 'to'):
     setattr(Addresses, "del_%s" % atype, del_multi)
 
     # To retain backwards compatibility:
-    setattr(Addresses, "get%s" % atype.title(), get_multi)
-    setattr(Addresses, "set%s" % atype.title(), set_multi)
-    setattr(Addresses, "del%s" % atype.title(), del_multi)
     if atype == 'all':
         Addresses.interfaces.add('addresses')
-        setattr(Addresses, "getAddresses", get_multi)
-        setattr(Addresses, "setAddresses", set_multi)
-        setattr(Addresses, "delAddresses", del_multi)
 
 
 register_stanza_plugin(Addresses, Address, iterable=True)

examples/adhoc_provider.py
@@ -68,7 +68,7 @@ class CommandBot(slixmpp.ClientXMPP):
                        session. Additional, custom data may be saved
                        here to persist across handler callbacks.
         """
-        form = self['xep_0004'].makeForm('form', 'Greeting')
+        form = self['xep_0004'].make_form('form', 'Greeting')
         form['instructions'] = 'Send a custom greeting to a JID'
         form.addField(var='greeting',
                       ftype='text-single',

examples/adhoc_user.py
@@ -94,7 +94,7 @@ class CommandUserBot(slixmpp.ClientXMPP):
         #          label="Your greeting" />
         # </x>
 
-        form = self['xep_0004'].makeForm(ftype='submit')
+        form = self['xep_0004'].make_form(ftype='submit')
         form.addField(var='greeting',
                       value=session['greeting'])
 

slixmpp/features/feature_bind/__init__.py
@@ -13,7 +13,3 @@ from slixmpp.features.feature_bind.stanza import Bind
 
 
 register_plugin(FeatureBind)
-
-
-# Retain some backwards compatibility
-feature_bind = FeatureBind

slixmpp/features/feature_mechanisms/__init__.py
@@ -16,7 +16,3 @@ from slixmpp.features.feature_mechanisms.stanza import Failure
 
 
 register_plugin(FeatureMechanisms)
-
-
-# Retain some backwards compatibility
-feature_mechanisms = FeatureMechanisms

slixmpp/features/feature_rosterver/__init__.py
@@ -13,7 +13,3 @@ from slixmpp.features.feature_rosterver.stanza import RosterVer
 
 
 register_plugin(FeatureRosterVer)
-
-
-# Retain some backwards compatibility
-feature_rosterver = FeatureRosterVer

slixmpp/features/feature_session/__init__.py
@@ -13,7 +13,3 @@ from slixmpp.features.feature_session.stanza import Session
 
 
 register_plugin(FeatureSession)
-
-
-# Retain some backwards compatibility
-feature_session = FeatureSession

slixmpp/features/feature_starttls/__init__.py
@@ -13,7 +13,3 @@ from slixmpp.features.feature_starttls.stanza import *
 
 
 register_plugin(FeatureSTARTTLS)
-
-
-# Retain some backwards compatibility
-feature_starttls = FeatureSTARTTLS

commit 5ef01ecdd1b3c8cfd912a135ccd85bcd8c3ff3f8

Fix XEP-0033
Re-add relevant stanza methods, broken in 7cd1cf32ae69

slixmpp/plugins/xep_0033/stanza.py
@@ -117,9 +117,12 @@ for atype in ('all', 'bcc', 'cc', 'noreply', 'replyroom', 'replyto', 'to'):
     setattr(Addresses, "set_%s" % atype, set_multi)
     setattr(Addresses, "del_%s" % atype, del_multi)
 
-    # To retain backwards compatibility:
     if atype == 'all':
         Addresses.interfaces.add('addresses')
+        setattr(Addresses, "get_addresses", get_multi)
+        setattr(Addresses, "set_addresses", set_multi)
+        setattr(Addresses, "del_addresses", del_multi)
+
 
 
 register_stanza_plugin(Addresses, Address, iterable=True)