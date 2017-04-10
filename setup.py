da5cb72d3a550a293986413b70ecf2a89558de77
mathieui <mathieui@mathieui.net>
2017-04-10 02:22:43 +0200

    Add XMPP classifier to setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,9 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Internet :: XMPP',
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
