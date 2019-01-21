poezio/slixmpp

commit c1562b76b273cc7aeaaa1cf8fddd3dca2c828c4d

slixmpp is Python 3.4.4+, remove check for channel binding.

slixmpp/features/feature_mechanisms/mechanisms.py
@@ -97,13 +97,7 @@ class FeatureMechanisms(BasePlugin):
                 jid = self.xmpp.requested_jid.bare
                 result[value] = creds.get('email', jid)
             elif value == 'channel_binding':
-                if hasattr(self.xmpp.socket, 'get_channel_binding'):
-                    result[value] = self.xmpp.socket.get_channel_binding()
-                else:
-                    log.debug("Channel binding not supported.")
-                    log.debug("Use Python 3.3+ for channel binding and " + \
-                              "SCRAM-SHA-1-PLUS support")
-                    result[value] = None
+                result[value] = self.xmpp.socket.get_channel_binding()
             elif value == 'host':
                 result[value] = creds.get('host', self.xmpp.requested_jid.domain)
             elif value == 'realm':

commit 41419a21613bd17f58965840340251872603a2e4

Fix authenticating on a non-TLS socket.
This was broken since c1562b76b273cc7aeaaa1cf8fddd3dca2c828c4d.

slixmpp/features/feature_mechanisms/mechanisms.py
@@ -97,7 +97,10 @@ class FeatureMechanisms(BasePlugin):
                 jid = self.xmpp.requested_jid.bare
                 result[value] = creds.get('email', jid)
             elif value == 'channel_binding':
-                result[value] = self.xmpp.socket.get_channel_binding()
+                if isinstance(self.xmpp.socket, (ssl.SSLSocket, ssl.SSLObject)):
+                    result[value] = self.xmpp.socket.get_channel_binding()
+                else:
+                    result[value] = None
             elif value == 'host':
                 result[value] = creds.get('host', self.xmpp.requested_jid.domain)
             elif value == 'realm':