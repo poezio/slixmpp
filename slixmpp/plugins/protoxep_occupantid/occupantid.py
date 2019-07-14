5541514581643a0047584a0e716cd879a91b608a
mathieui <mathieui@mathieui.net>
2019-07-14 13:25:22 +0200

    Do not add disco#info for occupantid, it’s a server thing

--- slixmpp/plugins/protoxep_occupantid/occupantid.py
+++ slixmpp/plugins/protoxep_occupantid/occupantid.py
@@ -15,13 +15,9 @@ from slixmpp.plugins.protoxep_occupantid import stanza
 class XEP_OccupantID(BasePlugin):
     name = 'protoxep_occupantid'
     description = 'XEP-XXXX: Anonymous unique occupant identifiers for MUCs'
-    dependencies = {'xep_0030'}
+    dependencies = set()
     stanza = stanza
 
     def plugin_init(self):
-        self.xmpp['xep_0030'].add_feature('urn:xmpp:occupant-id:0')
         register_stanza_plugin(Message, stanza.OccupantID)
         register_stanza_plugin(Presence, stanza.OccupantID)
-
-    def plugin_end(self):
-        self.xmpp['xep_0030'].remove_feature('urn:xmpp:occupant-id:0')
