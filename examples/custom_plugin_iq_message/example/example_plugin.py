4e542eb2aaf61cf0fe45d918fde915a9488ee9bd
guaz <maciej.pawlowski@mixbox.pl>
2019-10-30 20:34:04 +0100

    Initial commit for custom plugin tutorial files. Work still is in progress.

---
+++ examples/custom_plugin_iq_message/example/example_plugin.py
@@ -0,0 +1,111 @@
+import logging
+
+from slixmpp.xmlstream import ElementBase, ET, register_stanza_plugin
+
+from slixmpp import Iq
+from slixmpp import Message
+
+from slixmpp.plugins.base import BasePlugin
+
+from slixmpp.xmlstream.handler import Callback
+from slixmpp.xmlstream.matcher import StanzaPath
+
+log = logging.getLogger(__name__)
+
+class OurPlugin(BasePlugin):
+    def plugin_init(self):
+        self.description = "OurPluginExtension"   ##~ String data for Human readable and find plugin by another plugin with method.
+        self.xep = "ope"                          ##~ String data for Human readable and find plugin by another plugin with adding it into `slixmpp/plugins/__init__.py` to the `__all__` declaration with 'xep_OPE'. Otherwise it's just human readable annotation.
+        
+        #~ BasePlugin.post_init(self)                                  ##~ Initialize base plugin post init, to achieve this method and be sure to registred handlers and tags extension be saved.
+        self.xmpp.register_handler(
+                    Callback('ExampleGet Event:example_tag',    ##~ Name of this Callback
+                    StanzaPath('iq@type=get/example_tag'),      ##~ Handle only Iq with type get and example_tag
+                    self.__handle_get_iq))                      ##~ Method which catch proper Iq, should raise proper event for client.
+
+        self.xmpp.register_handler(
+                    Callback('ExampleResult Event:example_tag', ##~ Name of this Callback
+                    StanzaPath('iq@type=result/example_tag'),   ##~ Handle only Iq with type get and example_tag
+                    self.__handle_result_iq))                   ##~ Method which catch proper Iq, should raise proper event for client.
+
+        self.xmpp.register_handler(
+                    Callback('ExampleError Event:example_tag',  ##~ Name of this Callback
+                    StanzaPath('iq@type=error/example_tag'),    ##~ Handle only Iq with type get and example_tag
+                    self.__handle_error_iq))                    ##~ Method which catch proper Iq, should raise proper event for client.
+
+        self.xmpp.register_handler(
+                    Callback('ExampleMessage Event:example_tag',##~ Name of this Callback
+                    StanzaPath('message/example_tag'),          ##~ Handle only Iq with type get and example_tag
+                    self.__handle_message))                     ##~ Method which catch proper Iq, should raise proper event for client.
+
+        register_stanza_plugin(Iq, ExampleTag)                  ##~ Register tags extension for Iq object, otherwise iq['example_tag'] will be string field instead container where we can manage our fields and create sub elements.
+        register_stanza_plugin(Message, ExampleTag)                  ##~ Register tags extension for Iq object, otherwise iq['example_tag'] will be string field instead container where we can manage our fields and create sub elements.
+
+    # All iq types are: get, set, error, result
+    def __handle_get_iq(self, iq):
+        # Do something with iq
+        self.xmpp.event('example_tag_get_iq', iq)           ##~ Call event which can be handled by clients to send or something other what you want.
+        
+    def __handle_result_iq(self, iq):
+        # Do something with iq
+        self.xmpp.event('example_tag_result_iq', iq)        ##~ Call event which can be handled by clients to send or something other what you want.
+
+    def __handle_error_iq(self, iq):
+        # Do something with iq
+        self.xmpp.event('example_tag_error_iq', iq)         ##~ Call event which can be handled by clients to send or something other what you want.
+
+    def __handle_message(self, msg):
+        # Do something with message
+        self.xmpp.event('example_tag_message', msg)          ##~ Call event which can be handled by clients to send or something other what you want.
+
+class ExampleTag(ElementBase):
+    name = "example_tag"                                        ##~ The name of the root XML element of that extension.
+    namespace = "https://example.net/our_extension"             ##~ The namespace our stanza object lives in, like <example_tag xmlns={namespace} (...)</example_tag>. You should change it for your own namespace
+
+    plugin_attrib = "example_tag"                               ##~ The name to access this type of stanza. In particular, given  a  registration  stanza,  the Registration object can be found using: stanza_object['example_tag'] now `'example_tag'` is name of ours ElementBase extension. And this should be that same as name.
+    
+    interfaces = {"boolean", "some_string"}                     ##~ A list of dictionary-like keys that can be used with the stanza object. For example `stanza_object['example_tag']` gives us {"another": "some", "data": "some"}, whenever `'example_tag'` is name of ours ElementBase extension.
+
+    def setup_from_string(self, string):
+        extension_tag_XML = ET.fromstring(string)
+        #~ if extension_tag_XML.tag == "iq":
+            #~ pass
+        self.xml = extension_tag_XML
+
+    def setup_from_file(self, path):
+        with open(path, "r") as file_xml:
+            extension_tag_XML = file_xml.read()
+        self.setup_from_string(extension_tag_XML)
+
+    def setup_from_lxml(self, lxml):
+        pass
+
+    def get_boolean(self):
+        return self.xml.attrib.get("boolean")
+
+    def get_some_string(self):
+        return self.xml.attrib.get("some_string")
+        
+    def get_text(self, text):
+        return self.xml.text
+
+    def set_boolean(self, boolean):
+        self.xml.attrib['boolean'] = str(boolean)
+
+    def set_some_string(self, some_string):
+        self.xml.attrib['some_string'] = some_string
+
+    def set_text(self, text):
+        self.xml.text = text
+
+    def fill_interfaces(self, boolean, some_string):
+        #Some validation if it is necessary
+        self.set_boolean(boolean)
+        self.set_some_string(some_string)
+    
+    def add_inside_tag(self, tag, attributes, text=""):
+        #If we want to fill with additionaly tags our element, then we can do it that way for example:
+        itemXML = ET.Element("{{{0:s}}}{1:s}".format(self.namespace, tag)) #~ Initialize ET with our tag, for example: <example_tag (...)> <inside_tag namespace="https://example.net/our_extension"/></example_tag>
+        itemXML.attrib.update(attributes) #~ There we add some fields inside tag, for example: <inside_tag namespace=(...) inner_data="some"/>
+        itemXML.text = text #~ Fill field inside tag, for example: <inside_tag (...)>our_text</inside_tag>
+        self.xml.append(itemXML) #~ Add that all what we set, as inner tag inside `example_tag` tag.
---
+++ examples/custom_plugin_iq_message/testing_script.py
@@ -0,0 +1,36 @@
+# /usr/bin/python3
+import subprocess
+import _thread
+import time
+
+
+def start_shell(shell_string):
+    subprocess.run(shell_string, shell=True, universal_newlines=True)
+
+if __name__ == "__main__":
+    #~ prefix = "xterm -e" # Separate terminal
+    prefix = ""
+    #~ postfix = " -d" # Debug
+    postfix = ""
+
+    sender_path = "./example/sender.py"
+    sender_jid = "SENDER_JID"
+    sender_password = "SENDER_PASSWORD"
+
+    responder_path = "./example/responder.py"
+    responder_jid = "RESPONDER_JID"
+    responder_password = "RESPONDER_PASSWORD"
+
+    SENDER_TEST = (" ".join([prefix, "python3", sender_path, "-j", sender_jid, "-p", sender_password, postfix],)
+    RESPON_TEST = (" ".join([prefix, "python3", responder_path, "-j", responder_jid, "-p", responder_password, postfix],)
+    
+    # Create two threads as follows
+    try:
+        _thread.start_new_thread( start_shell, SENDER_TEST )
+        time.sleep(6)
+        _thread.start_new_thread( start_shell, RESPON_TEST )
+        while True:
+            time.sleep(0.5)
+    except:
+       print ("Error: unable to start thread")
+
