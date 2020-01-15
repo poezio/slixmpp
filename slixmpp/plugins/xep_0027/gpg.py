20cdfa397f3001bcdaeaeb56fba2964a35ef8e34
senior <senior@cryptex.net>
2020-01-15 14:29:27 +0300

    added passphrase field to unlock secret

--- slixmpp/plugins/xep_0027/gpg.py
+++ slixmpp/plugins/xep_0027/gpg.py
@@ -45,7 +45,8 @@ class XEP_0027(BasePlugin):
         'gpg_home': '',
         'use_agent': True,
         'keyring': None,
-        'key_server': 'pgp.mit.edu'
+        'key_server': 'pgp.mit.edu',
+        'passphrase': ''
     }
 
     def plugin_init(self):
@@ -110,7 +111,7 @@ class XEP_0027(BasePlugin):
                    '\n' + \
                    '%s\n' + \
                    '-----END PGP MESSAGE-----\n'
-        dec = self.gpg.decrypt(template % data)
+        dec = self.gpg.decrypt(template % data, passphrase=self.passphrase)
         return dec.data
 
     def verify(self, data, sig, jid=None):
