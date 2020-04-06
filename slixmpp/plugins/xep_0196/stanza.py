c3fbc6cb80902102a9f73ffab72e0ebd52c912af
Maxime “pep” Buquet <pep@bouah.net>
2020-04-06 12:57:02 +0200

    xep_0196: Use correct tag local name (thanks ivucica)
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- slixmpp/plugins/xep_0196/stanza.py
+++ slixmpp/plugins/xep_0196/stanza.py
@@ -11,10 +11,9 @@ from slixmpp.xmlstream import ElementBase, ET
 
 class UserGaming(ElementBase):
 
-    name = 'gaming'
+    name = 'game'
     namespace = 'urn:xmpp:gaming:0'
     plugin_attrib = 'gaming'
     interfaces = {'character_name', 'character_profile', 'name',
                   'level', 'server_address', 'server_name', 'uri'}
     sub_interfaces = interfaces
-
