3903d002c1e733126ab8362644e4771ac87f856f
Maxime “pep” Buquet <pep@bouah.net>
2020-05-27 22:36:28 +0200

    XEP-0421: add has_feature helper
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- slixmpp/plugins/xep_0421/stanza.py
+++ slixmpp/plugins/xep_0421/stanza.py
@@ -9,6 +9,9 @@
 from slixmpp.xmlstream import ElementBase
 
 
+NS = 'urn:xmpp:occupant-id:0'
+
+
 class OccupantId(ElementBase):
     '''
     An Occupant-id tag.
@@ -31,5 +34,5 @@ class OccupantId(ElementBase):
     '''
 
     name = 'occupant-id'
-    namespace = 'urn:xmpp:occupant-id:0'
+    namespace = NS
     interface = {'id'}
