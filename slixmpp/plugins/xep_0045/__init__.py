9cabba224addc4384e563ef4bd6d32a85cf26bf2
Maxime “pep” Buquet <pep@bouah.net>
2020-05-29 17:43:45 +0200

    XEP-0045: register_stanza_plgin(Message, MUCMessage)
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- slixmpp/plugins/xep_0045/__init__.py
+++ slixmpp/plugins/xep_0045/__init__.py
@@ -9,6 +9,6 @@
 from slixmpp.plugins import register_plugin
 from slixmpp.plugins.xep_0045 import stanza
 from slixmpp.plugins.xep_0045.muc import XEP_0045
-from slixmpp.plugins.xep_0045.stanza import MUCPresence
+from slixmpp.plugins.xep_0045.stanza import MUCPresence, MUCMessage
 
 register_plugin(XEP_0045)
