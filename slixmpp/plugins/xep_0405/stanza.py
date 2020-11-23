12c3b067b41c670cc3418b905b6e46d84511d194
mathieui <mathieui@mathieui.net>
2020-11-23 21:12:08 +0100

    XEP-0405: MIX-PAM

---
+++ slixmpp/plugins/xep_0405/stanza.py
@@ -0,0 +1,50 @@
+"""
+    Slixmpp: The Slick XMPP Library
+    Copyright (C) 2020 Nathanael "Mathieu Pasquet <mathieui@mathieui.net>
+    This file is part of Slixmpp.
+
+    See the file LICENSE for copying permissio
+"""
+
+from slixmpp import JID
+from slixmpp.stanza import (
+    Iq,
+)
+from slixmpp.xmlstream import (
+    ElementBase,
+    register_stanza_plugin,
+)
+
+from slixmpp.plugins.xep_0369.stanza import (
+    Join,
+    Leave,
+)
+
+NS = 'urn:xmpp:mix:pam:2'
+
+
+class ClientJoin(ElementBase):
+    namespace = NS
+    name = 'client-join'
+    plugin_attrib = 'client_join'
+    interfaces = {'channel'}
+
+    def set_channel(self, value: JID):
+        self.xml.attrib['channel'] = str(value)
+
+
+class ClientLeave(ElementBase):
+    namespace = NS
+    name = 'client-leave'
+    plugin_attrib = 'client_leave'
+    interfaces = {'channel'}
+    def set_channel(self, value: JID):
+        self.xml.attrib['channel'] = str(value)
+
+
+def register_plugins():
+    register_stanza_plugin(Iq, ClientJoin)
+    register_stanza_plugin(ClientJoin, Join)
+
+    register_stanza_plugin(Iq, ClientLeave)
+    register_stanza_plugin(ClientLeave, Leave)
