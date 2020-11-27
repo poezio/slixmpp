5cf53b12cc6f771f03efb10e768751e54bb31e00
mathieui <mathieui@mathieui.net>
2020-11-27 18:34:31 +0100

    Update protoxep_reactions to XEP-0444

--- slixmpp/plugins/xep_0444/reactions.py
+++ slixmpp/plugins/xep_0444/reactions.py
@@ -1,26 +1,28 @@
 """
     Slixmpp: The Slick XMPP Library
-    Copyright (C) 2019 Mathieu Pasquet
+    Copyright (C) 2020 Mathieu Pasquet
     This file is part of Slixmpp.
 
     See the file LICENSE for copying permission.
 """
 from typing import Iterable
 
+from slixmpp import JID
 from slixmpp.plugins import BasePlugin
 from slixmpp.stanza import Message
 from slixmpp.xmlstream import register_stanza_plugin
 from slixmpp.xmlstream.matcher import MatchXMLMask
 from slixmpp.xmlstream.handler import Callback
 
-from slixmpp.plugins.protoxep_reactions import stanza
+from slixmpp.plugins.xep_0444 import stanza
 
 
-class XEP_Reactions(BasePlugin):
-    name = 'protoxep_reactions'
-    description = 'XEP-XXXX: Message Reactions'
-    dependencies = {'xep_0030'}
+class XEP_0444(BasePlugin):
+    name = 'xep_0444'
+    description = 'XEP-0444: Message Reactions'
+    dependencies = {'xep_0030', 'xep_0334'}
     stanza = stanza
+    namespace = stanza.NS
 
     def plugin_init(self):
         self.xmpp.register_handler(
@@ -30,25 +32,32 @@ class XEP_Reactions(BasePlugin):
                 self._handle_reactions,
             )
         )
-        self.xmpp['xep_0030'].add_feature('urn:xmpp:reactions:0')
         register_stanza_plugin(Message, stanza.Reactions)
+        register_stanza_plugin(stanza.Reactions, stanza.Reaction, iterable=True)
+
+    def session_bind(self):
+        self.xmpp['xep_0030'].add_feature(stanza.NS)
 
     def plugin_end(self):
         self.xmpp.remove_handler('Reaction received')
-        self.xmpp['xep_0030'].remove_feature('urn:xmpp:reactions:0')
+        self.xmpp['xep_0030'].remove_feature(stanza.NS)
 
     def _handle_reactions(self, message: Message):
         self.xmpp.event('reactions', message)
 
+    def send_reactions(self, to: JID, to_id: str, reactions: Iterable[str], store=True):
+        """Send reactions related to a message"""
+        msg = self.xmpp.make_message(mto=to)
+        self.set_reactions(msg, to_id, reactions)
+        if store:
+            msg.enable('store')
+        msg.send()
+
     @staticmethod
     def set_reactions(message: Message, to_id: str, reactions: Iterable[str]):
-        """
-        Add reactions to a Message object.
-        """
-        reactions_stanza = stanza.Reactions()
-        reactions_stanza['to'] = to_id
+        """Add reactions to a Message object."""
+        message['reactions']['id'] = to_id
         for reaction in reactions:
             reaction_stanza = stanza.Reaction()
             reaction_stanza['value'] = reaction
-            reactions_stanza.append(reaction_stanza)
-        message.append(reactions_stanza)
+            message['reactions'].append(reaction_stanza)
---
+++ tests/test_stanza_xep_0444.py
@@ -0,0 +1,44 @@
+"""
+    Slixmpp: The Slick XMPP Library
+    Copyright (C) 2020 Mathieu Pasquet
+    This file is part of Slixmpp.
+
+    See the file LICENSE for copying permission.
+"""
+
+import unittest
+from slixmpp import Message
+from slixmpp.test import SlixTest
+from slixmpp.plugins.xep_0444 import XEP_0444
+import slixmpp.plugins.xep_0444.stanza as stanza
+from slixmpp.xmlstream import register_stanza_plugin
+
+
+class TestReactions(SlixTest):
+
+    def setUp(self):
+        register_stanza_plugin(Message, stanza.Reactions)
+        register_stanza_plugin(stanza.Reactions, stanza.Reaction)
+
+    def testCreateReactions(self):
+        """Testing creating Reactions."""
+
+        xmlstring = """
+          <message>
+              <reactions xmlns="urn:xmpp:reactions:0" id="abcd">
+                  <reaction>😃</reaction>
+                  <reaction>🤗</reaction>
+              </reactions>
+          </message>
+        """
+
+        msg = self.Message()
+        msg['reactions']['id'] = 'abcd'
+        msg['reactions'].set_values(['😃', '🤗'])
+
+        self.check(msg, xmlstring, use_values=False)
+
+        self.assertEqual({'😃', '🤗'}, msg['reactions'].get_values())
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestReactions)
