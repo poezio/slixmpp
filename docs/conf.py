155fc58a22d631746436a81119a94c80d6ea2d2b
mathieui <mathieui@mathieui.net>
2020-12-10 19:21:56 +0100

    docs: add sphinx_autodoc_typehints dep

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,11 @@ release = ".".join(version.split(".")[0:2])
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx',
+    'sphinx_autodoc_typehints',
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
