a1bb64c54f432aa61a0898ad134f185c95d2e848
mathieui <mathieui@mathieui.net>
2021-02-13 18:18:23 +0100

    XEP-0363: Types, docs, and new-style kwargs

--- slixmpp/plugins/xep_0363/__init__.py
+++ slixmpp/plugins/xep_0363/__init__.py
@@ -1,4 +1,3 @@
-
 # slixmpp: The Slick XMPP Library
 # Copyright (C) 2018 Emmanuel Gil Peyrot
 # This file is part of slixmpp.
@@ -6,6 +5,12 @@
 from slixmpp.plugins.base import register_plugin
 
 from slixmpp.plugins.xep_0363.stanza import Request, Slot, Put, Get, Header
-from slixmpp.plugins.xep_0363.http_upload import XEP_0363
+from slixmpp.plugins.xep_0363.http_upload import (
+    XEP_0363,
+    UploadServiceNotFound,
+    FileTooBig,
+    HTTPError,
+    FileUploadError,
+)
 
 register_plugin(XEP_0363)
