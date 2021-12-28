c7e6fef646566b880b3ded849ecb299f0e048b0e
Maxime “pep” Buquet <pep@bouah.net>
2021-12-28 18:19:38 +0100

    Make Xmlstream.ca_certs an Optional[Path] instead of Optional[str]
    Signed-off-by: Maxime “pep” Buquet <pep@bouah.net>

--- slixmpp/xmlstream/xmlstream.py
+++ slixmpp/xmlstream/xmlstream.py
@@ -33,6 +33,7 @@ import socket as Socket
 import ssl
 import weakref
 import uuid
+from pathlib import Path
 
 from contextlib import contextmanager
 import xml.etree.ElementTree as ET
@@ -161,7 +162,7 @@ class XMLStream(asyncio.BaseProtocol):
     #:
     #:     On Mac OS X, certificates in the system keyring will
     #:     be consulted, even if they are not in the provided file.
-    ca_certs: Optional[str]
+    ca_certs: Optional[Path]
 
     #: Path to a file containing a client certificate to use for
     #: authenticating via SASL EXTERNAL. If set, there must also
@@ -760,7 +761,7 @@ class XMLStream(asyncio.BaseProtocol):
                           self.certfile, self.keyfile)
         if self.ca_certs is not None:
             self.ssl_context.verify_mode = ssl.CERT_REQUIRED
-            self.ssl_context.load_verify_locations(cafile=self.ca_certs)
+            self.ssl_context.load_verify_locations(cafile=str(self.ca_certs))
 
         return self.ssl_context
 
